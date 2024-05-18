adfa2a1da12cf90d543fa95c23e740e6c37725f6
ElikplimD <kpodorinda@gmail.com>
2024-05-18 00:58:19 -0700

    Update arrayManipulation.js
    Task 2 completed

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -11,4 +11,22 @@ function processArray(numbers) {
 
 const inputNumbers = [1, 2, 3, 4, 5];
 const resultNumbers = processArray(inputNumbers);
-console.log(resultNumbers); 
+console.log(resultNumbers);
+
+
+
+//TASK 2
+function formatArrayStrings(strings, numbers) {
+  return strings.map((str, i) => {
+    if (numbers[i] % 2 === 0) {
+      return str.toUpperCase();
+    } else {
+      return str.toLowerCase();
+    }
+  });
+}
+
+const inputStrings = ["school", "church", "farm", "mosque", "shop"];
+const resultStrings = formatArrayStrings(inputStrings, resultNumbers);
+console.log(resultStrings);
+
