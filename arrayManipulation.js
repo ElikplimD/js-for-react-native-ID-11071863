6ba91cf991fd871e2d758ab2f68cb5a679dcd907
ElikplimD <kpodorinda@gmail.com>
2024-05-18 01:04:37 -0700

    TASK 3

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -30,3 +30,19 @@ const inputStrings = ["school", "church", "farm", "mosque", "shop"];
 const resultStrings = formatArrayStrings(inputStrings, resultNumbers);
 console.log(resultStrings);
 
+
+//TASK 3
+function createUserProfiles(names, modifiedNames) {
+  return names.map((name, i) => {
+    return {
+      originalName: name,
+      modifiedName: modifiedNames[i],
+      id: i + 1,
+    };
+  });
+}
+
+const inputNames = (inputStrings);
+const resultNames = (resultStrings);
+const resultProfiles = createUserProfiles(inputStrings, resultStrings);
+console.log(resultProfiles);
