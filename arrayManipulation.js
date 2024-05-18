829322cbef626671833b594cf8112a80aaefb610
ElikplimD <kpodorinda@gmail.com>
2024-05-18 01:17:34 -0700

    corrections made to arrayManipulation file

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -31,18 +31,3 @@ const resultStrings = formatArrayStrings(inputStrings, resultNumbers);
 console.log(resultStrings);
 
 
-//TASK 3
-function createUserProfiles(names, modifiedNames) {
-  return names.map((name, i) => {
-    return {
-      originalName: name,
-      modifiedName: modifiedNames[i],
-      id: i + 1,
-    };
-  });
-}
-
-const inputNames = (inputStrings);
-const resultNames = (resultStrings);
-const resultProfiles = createUserProfiles(inputStrings, resultStrings);
-console.log(resultProfiles);
